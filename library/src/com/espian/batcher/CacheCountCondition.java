0aa8313d71cbe36e03bec5bab06dc7d8b3e89709
Alex Curran <aml.curran@gmail.com>
2012-11-18 20:03:55 +0000

    Added CLSCs and lifecycle image

---
+++ library/src/com/espian/batcher/CacheCountCondition.java
@@ -0,0 +1,11 @@
+package com.espian.batcher;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexcurran
+ * Date: 18/11/2012
+ * Time: 19:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class CacheCountCondition {
+}
---
+++ library/src/com/espian/batcher/CacheListenerShotCondition.java
@@ -0,0 +1,11 @@
+package com.espian.batcher;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexcurran
+ * Date: 18/11/2012
+ * Time: 19:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class CacheListenerShotCondition {
+}
